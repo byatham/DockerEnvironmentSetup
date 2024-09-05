0892ca5b5610c7514381f2e4d880ae151ca3fea6
byatham <balaiahy23@gmail.com>
2024-09-05 13:42:28 +0530

    docker instances for local setup

---
+++ src/main/java/com/sd/tech/DockerEnvironmentSetupApplication.java
@@ -0,0 +1,13 @@
+package com.sd.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerEnvironmentSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerEnvironmentSetupApplication.class, args);
+	}
+
+}
